c4bf01c8204d622aab8beb858b7180d7a8225c0c
Nikita <kretov995@gmail.com>
2017-10-27 17:01:51 +0300

    fast juphub isntallation + provisisioning

---
+++ init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#install dependencies 
+apt update
+apt install -y python3 npm nodejs-legacy python3-pip
+npm install -g configurable-http-proxy
+
+#install juphub
+pip3 install jupyterhub
+pip3 install IPython jupyter_client
+pip3 install dockerspawner
+pip3 install netifaces
+
+#necesary config options
+cat << EOF > /home/ubuntu/jupyterhub_config.py
+from IPython.utils.localinterfaces import public_ips
+import netifaces
+docker0 = netifaces.ifaddresses('docker0')
+docker0_ipv4 = docker0[netifaces.AF_INET][0]
+
+c.JupyterHub.confirm_no_ssl = True
+c.JupyterHub.spawner_class = 'dockerspawner.SystemUserSpawner'
+c.JupyterHub.hub_ip = docker0_ipv4['addr']
+EOF
+
+#pull singleuser server
+docker pull jupyterhub/singleuser:0.8
+
+#add test user
+adduser --disabled-password --gecos "" stud1
+
+usermod -a -G docker ubuntu
+usermod -a -G docker stud1
+usermod -a -G sudo stud1
+
+reboot
---
+++ main.tf
@@ -0,0 +1,96 @@
+provider "aws" {
+    region =  "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+}
+
+variable "server_port" {
+    default = 22
+}
+variable "local_ip" {
+    default = "109.252.45.3"
+}
+
+resource "aws_instance" "test-serv" {
+    ami = "ami-05ed6813"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    key_name = "model"
+
+    root_block_device  {
+        volume_size = 15
+    }
+
+    provisioner "file" {
+    source      = "init.sh"
+    destination = "./init.sh"
+    connection {
+        type     = "ssh"
+        user     = "ubuntu"
+      }
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo chmod +x ./init.sh",
+            "sudo ./init.sh",
+        ]
+        connection {
+        type     = "ssh"
+        user     = "ubuntu"
+      }
+    }
+
+    tags {    
+        Name = "test-serv" 
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port = "${var.server_port}"
+        to_port = "${var.server_port}"
+        protocol = "tcp"
+        cidr_blocks = ["${var.local_ip}/32"]
+    }   
+    ingress {
+        from_port = "80"
+        to_port = "80"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = "8000"
+        to_port = "8000"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = "443"
+        to_port = "443"
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+    
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+output "public_ip" {
+    value = "${aws_instance.test-serv.public_ip}"
+}
+
+output "private_ip" {
+    value = "${aws_instance.test-serv.private_ip}"
+}
+
